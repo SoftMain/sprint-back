cd7fd14700889fa8bd6882eb33ed392303036dcd
Andrey Paliy <awbait.com@gmail.com>
2022-11-30 05:45:07 +0300

    CORS and dockerignore

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { AppModule } from './app.module';
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  });
   const config = new DocumentBuilder()
     .setTitle('SoftMain REST API')
     .setDescription('Документация REST API')
